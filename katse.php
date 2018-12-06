04d77129202b716ce97d44c8d1b3d5c941ee4996
mairisaar <mairi.saar@khk.ee>
2018-12-06 11:50:26 +0200

    Muudetud README.md faili sisu.

---
+++ katse.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mairi.saar
+ * Date: 06.12.2018
+ * Time: 11:08
+ */
+echo '
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <h1>PHP abil loodud HTML</h1>
+        </body>
+    </html>
+';
+
+
+
+?>
