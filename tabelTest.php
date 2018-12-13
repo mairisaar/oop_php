e88ce590b4d3e93af0083f5b68aad6816756e6e0
mairisaar <mairi.saar@khk.ee>
2018-12-13 10:58:20 +0200

    TabelTest. Lisatud testväljastus.

--- tabelTest.php
+++ tabelTest.php
@@ -10,6 +10,18 @@ require_once './Tabel.php';
 // loome tabeli objekti
 
 $lihttabel = new Tabel();
+
+//testväljastus
+
+var_dump($lihttabel);
+echo '<hr>';
+
+echo '<pre>';
+print_r($lihttabel);
+echo '</pre>';
+
+echo '<hr>';
+
 //lisame read sisuga
 $lihttabel->lisaRida(array(1, 2, 3));
 $lihttabel->lisaRida(array(4, 5, 6));
@@ -17,3 +29,13 @@ $lihttabel->lisaRida(array(7, 8, 9));
 //väljastamine
 $lihttabel->naitaTabel();
 
+//testväljastus
+
+var_dump($lihttabel);
+echo '<hr>';
+
+echo '<pre>';
+print_r($lihttabel);
+echo '</pre>';
+
+echo '<hr>';
