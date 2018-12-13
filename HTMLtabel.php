4472f58356b1ab533dcac4bfd5c36e13d106d635
mairisaar <mairi.saar@khk.ee>
2018-12-13 11:35:14 +0200

    TabelTest. HTMLtabel. Loodud HTMLtabel tüübi kirjeldus ja HTMLtabel tüüpi objekt.

--- HTMLtabel.php
+++ HTMLtabel.php
@@ -9,4 +9,30 @@ require_once './Tabel.php';
 class HTMLtabel extends Tabel
 {
 
+    /**
+     * HTMLtabel constructor.
+     */
+    public function __construct()
+    {
+        echo 'HTML';
+        parent::__construct();
+    }
+
+        /*
+         * vaata tabeli massiivi foreach ridade abil
+         * ja väljasta html tabeli kujul
+         * */
+    function naitaTabel()
+    {
+        echo '<table style="border-collapse: collapse; border: solid 1px #000000; width: 200px; text-align: center;">';
+        foreach ($this->tabel as $rida){
+            echo '<tr style="border-collapse: collapse; border: solid 1px #000000;">';
+            foreach ($rida as $element){
+                echo '<td style="border-collapse: collapse; border: solid 1px #000000;">'.$element.'</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
+
 }
