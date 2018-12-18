f2ef860324276c05a20c547a7d616e4f4d70202d
mairisaar <mairi.saar@khk.ee>
2018-12-18 10:49:53 +0200

    Vormid. Vormi töötlus.

--- vormiTootlus.php
+++ vormiTootlus.php
@@ -8,3 +8,21 @@
 echo '<pre>';
 print_r($_POST);
 echo '</pre>';
+
+if(empty($_POST)){
+    header('Location: vorm.php');
+//    echo '<a href="vorm.php">Logi sisse!</a>';
+}else {
+    foreach ($_POST as $elment){
+        if (empty($elment)){
+            echo 'Sisesta andmed!<br>';
+            echo '<a href="vorm.php">Logi sisse!</a>';
+            exit;
+        }
+    }
+    echo 'kasutajanimi = ' . $_POST['username'] . '<br>';
+    echo 'parool = ' . $_POST['passwd'] . '<br>';
+    echo '<hr>';
+
+    echo 'Tere tulemast, ' . $_POST['username'] . '<br>';
+}
