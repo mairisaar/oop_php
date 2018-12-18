9e94fda5d69f189d82af9e2c1e4cb7a991716e0e
mairisaar <mairi.saar@khk.ee>
2018-12-18 11:01:28 +0200

    Katse. Vormid. Vormi töötlus ühel lehel.

--- katse.php
+++ katse.php
@@ -6,6 +6,36 @@
  * Time: 11:08
  */
 
+function vorm(){
+    echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'">
+    <div>
+        <label>kasutajanimi :</label>
+        <input type="text" name="username">
+    </div>
+    <input type="submit" name="Saada!">
+</form>';
+}
+
+if(empty($_POST)){
+    vorm();
+}else {
+    foreach ($_POST as $elment){
+        if (empty($elment)){
+            echo 'Sisesta andmed!<br>';
+            echo '<a href="katse.php">Proovi uuesti!</a>';
+            exit;
+        }
+    }
+    echo 'kasutajanimi = ' . $_POST['username'] . '<br>';
+
+    echo '<hr>';
+
+    echo 'Tere tulemast, ' . $_POST['username'] . '<br>';
+};
+
+
+
+
 
 
 
@@ -18,6 +48,7 @@ echo '
             <link rel="stylesheet" href="katsestyle.css">
         </head>
         <body>';
+
 
 
 
