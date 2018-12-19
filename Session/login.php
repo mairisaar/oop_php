5fa38283cfe34fbbea28c891bcffce3ee8d6eb01
mairisaar <mairi.saar@khk.ee>
2018-12-19 11:29:44 +0200

    Login. Login_do. Logout. Loodud väljalogimine.

--- Session/login.php
+++ Session/login.php
@@ -1,5 +1,6 @@
 <?php
 session_start(); //alustame anonüümse sessiooniga
+session_regenerate_id();
 echo session_id();
 echo '<br>';
 echo '<div style="color: red">'.$_SESSION['viga'].'</div>';
--- Session/logout.php
+++ Session/logout.php
@@ -5,3 +5,8 @@
  * Date: 19.12.2018
  * Time: 11:17
  */
+
+session_start();
+//echo session_id();
+session_destroy();
+header('Location: login.php');
